0c461cb5bb5a584c1dedfb55a17d50bcff1da709
Sont3k <markus.residence@gmail.com>
2019-10-25 15:46:14 +0300

    Created Video.css and finished Video page layout

--- src/client/pages/VideoPage.js
+++ src/client/pages/VideoPage.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 
+import "../Styles/Video.css";
+
+import SEO from "../components/SEO";
+import Header from "../components/Header";
+import ScrollToTop from "../components/ScrollToTop";
+
 export default class VideoPage extends Component {
   render() {
-    return <div>Something great</div>;
-  }
-}
+    const lang = !this.props.location.pathname.includes("/ua");
+    let metaTags = this.props.getMetaResult && this.props.getMetaResult.result;
 
-export function loadDataVideo({ dispatch }, path) {
-    const lang = path.includes("/ua") ? "ua" : "ru";
-  
-    const promises = [dispatch(getSeoByUrl("video", lang))];
-  
-    return Promise.all(promises);
+    return (
+      <div className="Container">
+        <SEO
+          title={metaTags && metaTags.title}
+          description={metaTags && metaTags.description}
+          url={"https://suntown-ukraine.com/video"}
+        ></SEO>
+        <Header {...this.props} />
+        <ScrollToTop />
+        <div className="Content">
+          <video width="640" height="480" controls>
+            <source src="movie.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+          <p className="Description">Great video description</p>
+        </div>
+      </div>
+    );
+  }
 }
