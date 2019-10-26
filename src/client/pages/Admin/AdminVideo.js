b93cf7555a940c83218bc336b5142f9fda88b2c1
Sont3k <markus.residence@gmail.com>
2019-10-26 14:56:19 +0300

    Added file uploading logic

--- src/client/pages/Admin/AdminVideo.js
+++ src/client/pages/Admin/AdminVideo.js
@@ -2,7 +2,55 @@ import React, { Component } from "react";
 import "../../Styles/Admin.css";
 
 export default class AdminVideo extends Component {
-  state = {};
+  state = {
+    videoData: {
+      admin_video_description: "",
+      admin_video_videoTitle: ""
+    },
+    videoDataRu: {
+      admin_video_description: "",
+      admin_video_videoTitle: ""
+    },
+    isRu: false,
+    file: "Файл не выбран"
+  };
+
+  onChange = e => {
+    !this.state.isRu
+      ? this.setState({
+          ...this.state,
+          videoData: {
+            ...this.state.videoData,
+            [e.target.id]: e.target.value
+          }
+        })
+      : this.setState({
+          ...this.state,
+          videoDataRu: {
+            ...this.state.videoDataRu,
+            [e.target.id]: e.target.value
+          }
+        });
+  };
+
+  getFiles = e => {
+    const file = document.getElementById(e.target.id).files;
+    if (file.length > 0) {
+      if (this.state.isRu) {
+        this.setState({ file: "Файл выбран" });
+        return;
+      } else {
+        this.setState({ file: "Файл обраний" });
+        return;
+      }
+    }
+
+    if (this.state.isRu) {
+      this.setState({ file: "Файл не выбран" });
+    } else {
+      this.setState({ file: "Файл необраний" });
+    }
+  };
 
   render() {
     return (
@@ -14,12 +62,12 @@ export default class AdminVideo extends Component {
               className="admin_gallery_input"
               type="text"
               id="admin_video_description"
-              // value={
-              //   this.state.isRu
-              //     ? this.state.galleryDataRu.admin_gallery_description
-              //     : this.state.galleryData.admin_gallery_description
-              // }
-              // onChange={this.onChange}
+              value={
+                this.state.isRu
+                  ? this.state.videoDataRu.admin_video_description
+                  : this.state.videoData.admin_video_description
+              }
+              onChange={this.onChange}
             />
           </div>
 
@@ -30,8 +78,8 @@ export default class AdminVideo extends Component {
                 className="admin_gallery_input"
                 type="text"
                 id="admin_video_videoTitle"
-                // value={this.state.galleryData.admin_gallery_imageTitle}
-                // onChange={this.onChange}
+                value={this.state.videoData.admin_video_videoTitle}
+                onChange={this.onChange}
               />
             </div>
           ) : null}
@@ -40,14 +88,14 @@ export default class AdminVideo extends Component {
             <label className="admin_input_line">
               <h2>Файл</h2>
               <span className="admin_custom_file_selector">
-                {/* {this.state.file} */}
+                {this.state.file}
               </span>
               <input
                 className="admin_file_selector"
                 type="file"
-                accept="image/*, video/mp4"
+                accept="video/mp4"
                 id="admin_video_file"
-                // onChange={this.getFiles}
+                onChange={this.getFiles}
               />
             </label>
           ) : null}
